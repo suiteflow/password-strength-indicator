a4ed2171665e751324202ba165317492af5ea542
Aaron Grieve <127838213+suiteflow@users.noreply.github.com>
2023-05-29 09:07:02 +0100

    Update passwordstrength.js

--- passwordstrength.js
+++ passwordstrength.js
@@ -2,11 +2,15 @@ var passwordFields = document.querySelectorAll('[data-password-field="true"]');
 var passwordStrengthIndicators = document.querySelectorAll('[data-password-strength-indicator="true"]');
 
 passwordFields.forEach((passwordField, index) => {
+  var originalStrengthText = passwordStrengthIndicators[index].textContent; // Store the original text
+
   passwordField.addEventListener('input', function (e) {
     var password = e.target.value;
     var strengthText = '';
 
-    if (password.length < 8) {
+    if (password === '') {
+      strengthText = originalStrengthText; // Set the strengthText back to the original text
+    } else if (password.length < 8) {
       strengthText = 'Too short';
     } else if (!/[A-Z]/.test(password)) {
       strengthText = 'Must contain uppercase letters';
